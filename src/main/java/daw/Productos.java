c3222dd56d8258e5ed205425a5eb9047fc44ed72
dnavmoy <dnavmoy119@g.educaand.es>
2024-01-16 12:06:28 +0100

    con enum

---
+++ src/main/java/daw/Productos.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw;
+
+/**
+ *
+ * @author daniel
+ */
+public class Productos{
+    
+    public final int ID;
+    public String descripcion;
+    public String nombre;
+    public Comida comida;
+    public Bebida bebida;
+    public Postre postre;
+    
+    
+    
+    
+    
+}
