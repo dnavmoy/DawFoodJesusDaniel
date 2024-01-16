da30d4ff3d85cc8c9e9800c320cbed6d7b2dbeaf
jrommir834e <jrommir834e@g.educaand.es>
2024-01-16 12:33:18 +0100

    Creación Record, con datos clientes y los datos de su tarjeta(Cambiar luego los datos de la tarjeta a una clase si nos viene mejor)

---
+++ src/main/java/daw/Clientes.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Record.java to edit this template
+ */
+package daw;
+
+import java.util.Date;
+
+/**
+ *
+ * @author jesus
+ */
+public record Clientes() {
+
+    //Atributos clientes
+    public static String nombre;
+    public static int edad;
+    
+    //Datos tarjeta, despues si hay tiempo de hace una clase Tarjeta aparte 
+    public static int numTarjeta;
+    public static int cvv;
+    public static Date fechaVencimiento;
+    public static double saldoTarjeta;        
+            
+    
+    
+            
+}
