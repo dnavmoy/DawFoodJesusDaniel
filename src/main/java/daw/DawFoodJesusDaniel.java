c062938fa437d79e7d1d2bcdba0eb1814c3167a9
dnavmoy <dnavmoy119@g.educaand.es>
2024-01-16 11:01:52 +0100

    v0 añado resumen proyecto

--- src/main/java/daw/DawFoodJesusDaniel.java
+++ src/main/java/daw/DawFoodJesusDaniel.java
@@ -8,6 +8,33 @@ package daw;
  *
  * @author daniel
  */
+
+
+
+//
+//menu desplegable-> seleccionar tpv
+//->  usuario, administrador,
+//usuario-> comida,bebida,postre
+//		--> tres subcategorias
+//	opcion-> pedir, consultar precio
+//todo momento atras-inicio-añadir producto con cantidad.
+//
+//
+//administrador-> contraseña -> menu cambios
+//
+//pasarela de pago
+//
+//clases
+//
+//array clientes-> con tarjetas guardadas
+//array productos->comida,bebida,postres->subclases
+//
+//arraylist pedidos
+//arraylist- lista pedidos
+//
+//tpv- constraseñasadministrador 
+
+
 public class DawFoodJesusDaniel {
 
     public static void main(String[] args) {
