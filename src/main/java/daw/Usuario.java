f64ed6fd18576b786aef25ed9218d10c0aed68a1
dnavmoy <dnavmoy119@g.educaand.es>
2024-01-22 10:16:07 +0100

    intento clase usuario para meter metodo alli

--- src/main/java/daw/Usuario.java
+++ src/main/java/daw/Usuario.java
@@ -4,6 +4,10 @@
  */
 package daw;
 
+import static daw.DawFoodJesusDaniel.respuestaBoton;
+import java.time.Instant;
+import java.util.Date;
+
 /**
  *
  * @author daniel
@@ -13,6 +17,144 @@ public class Usuario {
     public static boolean Usuario(){
         boolean atras=true;
         
+        
+        
+                                Carrito carrito= new Carrito();
+                                //ArrayList<Productos> carrito = new ArrayList<>();
+                                do {
+                                    String[] menuUsuario = {"ATRAS", "COMIDA", "BEBIDA", "POSTRE", "VER CARRITO", "PAGAR"};
+                                    int usuario = respuestaBoton(menuUsuario);
+                                    switch (usuario) {
+                                        case 1:
+
+                                            do {
+                                                String[] menuComida = {"ATRAS", "ENTRANTES", "PRIMEROS", "SEGUNDOS"};
+                                                int comida = respuestaBoton(menuComida);
+                                                switch (comida) {
+                                                    case 1:
+
+                                                        do {
+
+                                                            Productos objeto = new Productos(-1, "", 0, 0, 0, Comidas.ENTRANTES);
+                                                            atras = Pruebas.pedir(lista, carrito, objeto);
+
+                                                        } while (atras);
+                                                        atras = true;
+                                                        break;
+
+                                                    case 2:
+                                                        do {
+                                                            Productos objeto = new Productos(-1, "", 0, 0, 0, Comidas.PRIMEROS);
+                                                            atras = Pruebas.pedir(lista, carrito, objeto);
+
+                                                        } while (atras);
+                                                        atras = true;
+                                                        break;
+                                                    case 3:
+                                                        Productos objeto = new Productos(-1, "", 0, 0, 0, Comidas.SEGUNDOS);
+                                                        atras = Pruebas.pedir(lista, carrito, objeto);
+
+                                                        break;
+
+                                                    case 0:
+                                                        atras = false;
+
+                                                }
+
+                                            } while (atras);
+                                            atras = true;
+                                            break;
+                                        case 2:
+
+                                            do {
+                                                String[] bebidaarray = {"ATRAS", "ALCOHOLICA", "Con Gas", "Sin Gas"};
+                                                int bebida = respuestaBoton(bebidaarray);
+
+                                                switch (bebida) {
+                                                    case 1:
+                                                        do {
+                                                            Productos objeto = new Productos(-1, "", 0, 0, 0, Bebidas.ALCOHOLICA);
+                                                            atras = Pruebas.pedir(lista, carrito, objeto);
+
+                                                        } while (atras);
+                                                        atras = true;
+                                                        break;
+                                                    case 2:
+                                                        do {
+                                                            Productos objeto = new Productos(-1, "", 0, 0, 0, Bebidas.CON_GAS);
+                                                            atras = Pruebas.pedir(lista, carrito, objeto);
+
+                                                        } while (atras);
+                                                        atras = true;
+                                                        break;
+                                                    case 3:
+                                                        do {
+                                                            Productos objeto = new Productos(-1, "", 0, 0, 0, Bebidas.SIN_GAS);
+                                                            atras = Pruebas.pedir(lista, carrito, objeto);
+
+                                                        } while (atras);
+                                                        atras = true;
+                                                        break;
+                                                    case 0:
+                                                        atras = false;
+
+                                                }
+
+                                            } while (atras);
+                                            atras = true;
+                                            break;
+                                        case 3:
+                                            do {
+                                                String[] menuPostre = {"ATRAS", "CALIENTE", "FRIO", "FRUTA"};
+                                                int postre = respuestaBoton(menuPostre);
+
+                                                switch (postre) {
+                                                    case 1:
+                                                        do {
+                                                            Productos objeto = new Productos(-1, "", 0, 0, 0, Postre.CALIENTE);
+                                                            atras = Pruebas.pedir(lista, carrito, objeto);
+
+                                                        } while (atras);
+                                                        atras = true;
+                                                        break;
+                                                    case 2:
+                                                        do {
+                                                            Productos objeto = new Productos(-1, "", 0, 0, 0, Postre.FRIO);
+                                                            atras = Pruebas.pedir(lista, carrito, objeto);
+
+                                                        } while (atras);
+                                                        atras = true;
+                                                        break;
+                                                    case 3:
+                                                        do {
+                                                            Productos objeto = new Productos(-1, "", 0, 0, 0, Postre.FRUTA);
+                                                            atras = Pruebas.pedir(lista, carrito, objeto);
+
+                                                        } while (atras);
+                                                        atras = true;
+                                                        break;
+                                                    case 0:
+                                                        atras = false;
+
+                                                }
+
+                                            } while (atras);
+                                            atras = true;
+                                            break;
+                                        case 4:
+                                            Pruebas.consultarProductos(carrito);
+                                            break;
+                                        case 5:
+                                            boolean pagado=Funciones.pasarelaPago(new Tarjeta(usuario, respTpv, Date.from(Instant.now()), usuario));
+                                            if (pagado){
+                                            tpv1.getVentas().add(carrito);
+                                            }
+                                            break;
+                                        case 0:
+                                            atras = false;
+                                    }
+                                } while (atras);
+                                
         
         return atras;
     }
