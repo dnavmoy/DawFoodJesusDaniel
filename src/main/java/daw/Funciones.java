19657a19a407262510965777d7e26ac01438150f
jrommir834e <jrommir834e@g.educaand.es>
2024-01-23 11:01:03 +0100

    lista tarjetas y pasarela de pago casi terminada

--- src/main/java/daw/Funciones.java
+++ src/main/java/daw/Funciones.java
@@ -4,6 +4,9 @@
  */
 package daw;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  *
  * @author jesus
@@ -13,7 +16,16 @@ public class Funciones {
 
     public static boolean pasarelaPago(Tarjeta tarjeta){
         boolean correcto=true;
-        //int numTarjeta=DawFoodJesusDaniel.respuestaJopt("Intoduce tarjeta");
+        int numTarjeta=DawFoodJesusDaniel.respuestaJopt("Intoduce tarjeta");
+        
+        for(int i=0;i<tarjetas().size();i++){
+            
+            if(numTarjeta==tarjeta.getNumTarjeta()){
+                //tarjeta.setSaldo(tarjeta.getSaldo()-Pruebas.pedi());
+            }
+            
+        }
+            
         
         
 
@@ -21,6 +33,20 @@ public class Funciones {
     }
     
     
+    public static ArrayList tarjetas () {
     
+    Tarjeta t1=new Tarjeta(1234,123, new Date(2025,12,30),1000);
+    Tarjeta t2=new Tarjeta(4321,321, new Date(2024,12,30),200);
+    Tarjeta t3=new Tarjeta(1357,135, new Date(2026,12,30),12000);
+    
+     ArrayList<Tarjeta> listaTarjeta = new ArrayList();
+     
+        listaTarjeta.add (t1);
+        listaTarjeta.add(t2);
+        listaTarjeta.add(t3);
+        
+        return listaTarjeta;
+    
+}
     
 }
