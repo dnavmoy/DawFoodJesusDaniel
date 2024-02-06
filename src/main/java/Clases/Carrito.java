cae7e11acb763beec2049f6abe8f992e9e8d7c0c
dnavmoy <dnavmoy119@g.educaand.es>
2024-02-06 10:34:25 +0100

    inicio cambio a map en carrito

--- src/main/java/Clases/Carrito.java
+++ src/main/java/Clases/Carrito.java
@@ -5,8 +5,9 @@
 package Clases;
 
 import Clases.Productos;
-import java.util.ArrayList;
-import java.util.Objects;
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  *
@@ -14,81 +15,17 @@ import java.util.Objects;
  */
 public class Carrito {
  
-    
-    private ArrayList<Productos> cesta;
-    private ArrayList<Integer> cantidad;
-
-    public Carrito() {
-        this.cesta = new ArrayList<Productos>();
-        this.cantidad = new ArrayList<Integer>();
-    }
-    
-    public Carrito(ArrayList<Productos> productos, ArrayList<Integer> cantidades) {
-        this.cesta = productos;
-        this.cantidad = cantidades;
-    }
-  
-    
-    public Carrito(Carrito copia){
-        this.cesta= copia.cesta;
-        this.cantidad=copia.cantidad;
-    }
-
-    public ArrayList<Productos> getCesta() {
-        return cesta;
-    }
-
-  
-
-    public void setCesta(ArrayList<Productos> cesta) {
-        this.cesta = cesta;
-    }
-
-    public ArrayList<Integer> getCantidad() {
-        return cantidad;
-    }
-
-    public void setCantidad(ArrayList<Integer> cantidad) {
-        this.cantidad = cantidad;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 97 * hash + Objects.hashCode(this.cesta);
-        hash = 97 * hash + Objects.hashCode(this.cantidad);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Carrito other = (Carrito) obj;
-        if (!Objects.equals(this.cesta, other.cesta)) {
-            return false;
-        }
-        return Objects.equals(this.cantidad, other.cantidad);
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Carrito{");
-        sb.append("cesta=").append(cesta);
-        sb.append(", cantidad=").append(cantidad);
-        sb.append('}');
-        return sb.toString();
-    }
+    private Map<Integer,Integer> carrito;
 
     
+    public Carrito(){
+        this.carrito=new HashMap<>();
+    }
+    
+
+
+
+   
 
     
 }
