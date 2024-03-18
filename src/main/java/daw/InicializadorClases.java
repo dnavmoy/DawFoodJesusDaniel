416fa108eec711a95aec9328dfbbd42071c4e42a
dnavmoy <dnavmoy119@g.educaand.es>
2024-03-18 09:39:28 +0100

    csvs listos

--- src/main/java/daw/InicializadorClases.java
+++ src/main/java/daw/InicializadorClases.java
@@ -78,6 +78,72 @@ public class InicializadorClases {
             listaDevolver.getListaProductos().add(temporal);
             
         }
+        lista=leerFichero("segundos.csv");
+        for (int i = 0; i < lista.size(); i++) {
+            //Separamos por , para obtener los datos de cada vehiculo
+            String[] array = lista.get(i).split(",");//Corta por cada coma
+            //Creamos un objeto producto y metemos los datos en cada campo
+            Productos temporal = new Productos(Integer.parseInt(array[0]),array[1], Double.parseDouble(array[2]), Double.parseDouble(array[3]), Integer.parseInt(array[4]), Comidas.SEGUNDOS);
+            listaDevolver.getListaProductos().add(temporal);
+            
+        }
+        lista=leerFichero("alcoholicas.csv");
+        for (int i = 0; i < lista.size(); i++) {
+            //Separamos por , para obtener los datos de cada vehiculo
+            String[] array = lista.get(i).split(",");//Corta por cada coma
+            //Creamos un objeto producto y metemos los datos en cada campo
+            Productos temporal = new Productos(Integer.parseInt(array[0]),array[1], Double.parseDouble(array[2]), Double.parseDouble(array[3]), Integer.parseInt(array[4]), Bebidas.ALCOHOLICA);
+            listaDevolver.getListaProductos().add(temporal);
+            
+        }
+        lista=leerFichero("gas.csv");
+        for (int i = 0; i < lista.size(); i++) {
+            //Separamos por , para obtener los datos de cada vehiculo
+            String[] array = lista.get(i).split(",");//Corta por cada coma
+            //Creamos un objeto producto y metemos los datos en cada campo
+            Productos temporal = new Productos(Integer.parseInt(array[0]),array[1], Double.parseDouble(array[2]), Double.parseDouble(array[3]), Integer.parseInt(array[4]), Bebidas.CON_GAS);
+            listaDevolver.getListaProductos().add(temporal);
+            
+        }
+        lista=leerFichero("singas.csv");
+        for (int i = 0; i < lista.size(); i++) {
+            //Separamos por , para obtener los datos de cada vehiculo
+            String[] array = lista.get(i).split(",");//Corta por cada coma
+            //Creamos un objeto producto y metemos los datos en cada campo
+            Productos temporal = new Productos(Integer.parseInt(array[0]),array[1], Double.parseDouble(array[2]), Double.parseDouble(array[3]), Integer.parseInt(array[4]), Bebidas.SIN_GAS);
+            listaDevolver.getListaProductos().add(temporal);
+            
+        }
+        
+        lista=leerFichero("calientes.csv");
+        for (int i = 0; i < lista.size(); i++) {
+            //Separamos por , para obtener los datos de cada vehiculo
+            String[] array = lista.get(i).split(",");//Corta por cada coma
+            //Creamos un objeto producto y metemos los datos en cada campo
+            Productos temporal = new Productos(Integer.parseInt(array[0]),array[1], Double.parseDouble(array[2]), Double.parseDouble(array[3]), Integer.parseInt(array[4]), Postre.CALIENTE);
+            listaDevolver.getListaProductos().add(temporal);
+            
+        }
+        
+        lista=leerFichero("frios.csv");
+        for (int i = 0; i < lista.size(); i++) {
+            //Separamos por , para obtener los datos de cada vehiculo
+            String[] array = lista.get(i).split(",");//Corta por cada coma
+            //Creamos un objeto producto y metemos los datos en cada campo
+            Productos temporal = new Productos(Integer.parseInt(array[0]),array[1], Double.parseDouble(array[2]), Double.parseDouble(array[3]), Integer.parseInt(array[4]), Postre.FRIO);
+            listaDevolver.getListaProductos().add(temporal);
+            
+        }
+        
+        lista=leerFichero("fruta.csv");
+        for (int i = 0; i < lista.size(); i++) {
+            //Separamos por , para obtener los datos de cada vehiculo
+            String[] array = lista.get(i).split(",");//Corta por cada coma
+            //Creamos un objeto producto y metemos los datos en cada campo
+            Productos temporal = new Productos(Integer.parseInt(array[0]),array[1], Double.parseDouble(array[2]), Double.parseDouble(array[3]), Integer.parseInt(array[4]), Postre.FRUTA);
+            listaDevolver.getListaProductos().add(temporal);
+            
+        }
         
         
 
@@ -86,27 +152,7 @@ public class InicializadorClases {
     
     
     
- public static ArrayProductos incializadorProductos(){
-     ArrayProductos lista=new ArrayProductos();
-        
-        Productos ensalada = new Productos(1, "ensalada", 1, 0.21, 10, Comidas.ENTRANTES);
-        Productos gazpacho = new Productos(2, "gazpacho", 15, 0.21, 10, Comidas.ENTRANTES);
-        Productos agua = new Productos(3, "agua", 1.5, 0.21, 10, Bebidas.SIN_GAS);
-        Productos carne = new Productos(4, "filete", 20, 0.21, 10, Comidas.PRIMEROS);
-        Productos cerveza = new Productos(5, "Cerveza", 3.2, 0.21, 10, Bebidas.ALCOHOLICA);
-        Productos manzana = new Productos(6, "Manzana", 0.75, 0.21, 7, Postre.FRUTA);
-        //ArrayList<Productos> lista = new ArrayList();
-
-        lista.getListaProductos().add(ensalada);
-        lista.getListaProductos().add(gazpacho);
-        lista.getListaProductos().add(agua);
-        lista.getListaProductos().add(carne);
-        lista.getListaProductos().add(cerveza);
-        lista.getListaProductos().add(manzana);
-        
-        return lista;
-     
- }   
+ 
 
     public static ArrayList<Tarjeta> tarjetas() {
         Tarjeta t1 = new Tarjeta(1234, 123, LocalDate.of(2025, 12, 30), 1);
